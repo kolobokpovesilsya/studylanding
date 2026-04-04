b4a4052c3a799056364d7d3306025d0142bf10a4
Test1 <test1@mail.com>
2026-04-04 17:26:53 +0300

    initialize project

---
+++ src/blocks/main.js
@@ -0,0 +1,10 @@
+ 
+// import { initHeader } from './blocks/header.js'; 
+
+// Инициализация всех блоков при загрузке DOM
+document.addEventListener('DOMContentLoaded', () => {
+  
+//   initHeader(); 
+  
+});
+ 
