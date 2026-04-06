51ea69a66c818cc4b63e739d60b69484e53880e8
Test1 <test1@mail.com>
2026-04-06 22:50:04 +0300

    Add slider engine

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
  
 // import { initHeader } from './blocks/header.js'; 
 
+import { Slider } from "./blocks/slider/slider";
+
 // Инициализация всех блоков при загрузке DOM
 document.addEventListener('DOMContentLoaded', () => {
   
 //   initHeader(); 
-  
+  const slider = new Slider({
+    selector:'.team__slider.slider',
+    
+  })
 });
  
