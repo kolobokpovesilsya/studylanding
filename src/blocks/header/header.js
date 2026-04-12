f09c40c4a30914870741c9923de620092298ba1b
Test1 <test1@mail.com>
2026-04-12 23:03:18 +0300

    add header animation

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,1 +1,21 @@
-export function initHeader() {}
+let prevScrollTop=0
+
+const scrollHandler=(e)=>{
+    const header = document.body.querySelector('header')
+    const scrollTop = document.body.scrollTop
+    const height = document.body.scrollHeight
+    console.log('===',height)
+    if(!scrollTop){
+        header.classList.remove('header--fill')
+        header.classList.remove('header--hide')
+    }else if(scrollTop>prevScrollTop){
+        header.classList.add('header--hide')
+    }else{
+        header.classList.remove('header--hide')
+        header.classList.add('header--fill')
+    }
+    prevScrollTop =scrollTop
+}
+export function initHeader() {
+    document.body.addEventListener('scroll',scrollHandler)
+}
