252edf6b66b63dbfeedf22bc23f977a3f1b2bef1
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-04-15 15:57:11 +0300

    Add mobile header animation with js

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,6 +1,8 @@
 let prevScrollTop = 0;
 const burgerBtn = document.querySelector(".header__burgerbutton");
 const header = document.body.querySelector("header");
+let initialHeaderHeight;
+let initialHeaderBackground;
 const scrollHandler = (e) => {
     const scrollTop = window.scrollY;
     if (!scrollTop) {
@@ -14,8 +16,52 @@ const scrollHandler = (e) => {
     }
     prevScrollTop = scrollTop;
 };
+
+function toggleExpand(endCallback, duration = 200) {
+    const startHeaderHeight = header.offsetHeight;
+    let startTime = performance.now();
+    const shouldExpand = window.innerHeight > startHeaderHeight;
+    let delta = window.innerHeight / 4;
+    if (!shouldExpand) {
+        delta *= -1;
+    }
+    function doExpand(currentTime) {
+        const elapsed = currentTime - startTime;
+        const factor = currentTime / startTime;
+        let currentHeaderHeight = header.offsetHeight + delta * factor;
+        let timeIsOver = elapsed > duration;
+        const ifHeaderHeightCompleted = shouldExpand
+            ? currentHeaderHeight > window.innerHeight
+            : currentHeaderHeight <= initialHeaderHeight;
+        const finishHeaderHeight = shouldExpand
+            ? window.innerHeight
+            : initialHeaderHeight;
+        if (ifHeaderHeightCompleted || timeIsOver) {
+            header.style.height = `${finishHeaderHeight}px`;
+            endCallback();
+        } else {
+            header.style.height = `${currentHeaderHeight}px`;
+            requestAnimationFrame(doExpand);
+        }
+    }
+    requestAnimationFrame(doExpand);
+}
 function clickBurgerMenu(e) {
-    header.classList.toggle("header--extended");
+    if (!header.classList.contains("header--extended")) {
+        initialHeaderHeight = header.offsetHeight;
+        initialHeaderBackground = header.style.background;
+        header.style.background = "var(--color-panel)";
+        toggleExpand(() => {
+            header.classList.toggle("header--extended");
+        });
+    } else {
+        toggleExpand(() => {
+            header.classList.toggle("header--extended");
+            setTimeout(() => {
+                header.style.background = initialHeaderBackground;
+            }, 200);
+        });
+    }
 }
 export function initHeader() {
     document.addEventListener("scroll", scrollHandler);
