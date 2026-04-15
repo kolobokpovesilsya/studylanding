076505fcff8f90d70527726aa7c4cb65bba330c9
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-04-15 11:30:46 +0300

    Add mobile header displaying variant

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,19 +1,23 @@
-let prevScrollTop=0
-
-const scrollHandler=(e)=>{
-    const header = document.body.querySelector('header')
-    const scrollTop = window.scrollY
-    if(!scrollTop){
-        header.classList.remove('header--fill')
-        header.classList.remove('header--hide')
-    }else if(scrollTop>prevScrollTop){
-        header.classList.add('header--hide')
-    }else{
-        header.classList.remove('header--hide')
-        header.classList.add('header--fill')
+let prevScrollTop = 0;
+const burgerBtn = document.querySelector(".header__burgerbutton");
+const header = document.body.querySelector("header");
+const scrollHandler = (e) => {
+    const scrollTop = window.scrollY;
+    if (!scrollTop) {
+        header.classList.remove("header--fill");
+        header.classList.remove("header--hide");
+    } else if (scrollTop > prevScrollTop) {
+        header.classList.add("header--hide");
+    } else {
+        header.classList.remove("header--hide");
+        header.classList.add("header--fill");
     }
-    prevScrollTop =scrollTop
+    prevScrollTop = scrollTop;
+};
+function clickBurgerMenu(e) {
+    header.classList.toggle("header--extended");
 }
 export function initHeader() {
-    document.addEventListener('scroll',scrollHandler)
+    document.addEventListener("scroll", scrollHandler);
+    burgerBtn?.addEventListener("click", clickBurgerMenu);
 }
