f079a2792e51c24c2d15f3e4cc90642e1a7f9869
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-04-17 16:26:35 +0300

    Animate brands section appearing

---
+++ src/blocks/brands/brands.js
@@ -0,0 +1,30 @@
+import { IntersectionAnimation } from "../../utils/intersectionAnimation";
+
+const brandsSection = document.querySelector(".brands__container");
+const brandsTitle = document.querySelector(".brands__title");
+const brandsItems = document.querySelectorAll(".brands__item");
+
+function onHide() {
+    brandsTitle.classList.add("brands__title--hide");
+    brandsItems.forEach((item) => {
+        item.classList.add("brands__item--hide");
+    });
+}
+function onIntersect() {
+    brandsTitle.classList.remove("brands__title--hide");
+    brandsTitle.classList.add("brands__title--show");
+    brandsItems.forEach((item, idx) => {
+        item.classList.remove("brands__item--hide");
+        item.style.setProperty("--delay", `${0.2 * (idx + 1)}s`);
+        item.classList.add("brands__item--show");
+    });
+}
+export function initBrands() {
+    const intersectionObserver = new IntersectionAnimation({
+        containerSelector: ".brands",
+        onHide,
+        onIntersect,
+    });
+
+    // intersectionObserver.observe(brandsSection);
+}
--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@
 
 import { initHeader } from "./blocks/header/header.js";
 import { initClassroom } from "./blocks/classroom/classroom.js";
+import { initBrands } from "./blocks/brands/brands.js";
 
 import { Slider } from "./blocks/slider/slider";
 
 document.addEventListener("DOMContentLoaded", () => {
     initHeader();
     initClassroom();
-    const slider = new Slider({
-        selector: ".team__slider.slider",
-    });
+    initBrands();
+    // const slider = new Slider({
+    //     selector: ".team__slider.slider",
+    // });
 });
