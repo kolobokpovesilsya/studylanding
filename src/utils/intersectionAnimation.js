2c06fad4b9b2fe4e8fa877c6744b016e136ac874
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-04-17 16:26:49 +0300

    Add intersection utils class

---
+++ src/utils/intersectionAnimation.js
@@ -0,0 +1,41 @@
+export class IntersectionAnimation {
+    constructor({
+        containerSelector,
+        getContainerSelector,
+        onHide,
+        onIntersect,
+        once = true,
+        threshold = 1,
+    }) {
+        const intersectionObserver = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        console.log("Элемент появился в зоне видимости!");
+                        // entry.target.style.opacity = "1";
+                        onIntersect();
+                        once && intersectionObserver.unobserve(entry.target);
+                    } else {
+                        if (once) {
+                            return;
+                        }
+                        onHide();
+                        console.log("Элемент скрылся");
+                    }
+                });
+            },
+            { threshold },
+        );
+        const selector = getContainerSelector
+            ? getContainerSelector()
+            : containerSelector;
+        const brandsSection = document.querySelector(selector);
+        intersectionObserver.observe(brandsSection);
+
+        if (onHide) {
+            onHide();
+        } else {
+            selector.style.opacity = 0;
+        }
+    }
+}
