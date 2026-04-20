f07e379836770a2291502aef21bf0e120f3f0a60
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-04-20 17:56:11 +0300

    remove dist

--- dist/src/blocks/intro/intro.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".intro__content").children;
-document.querySelectorAll(".intro__popup");
--- dist/src/blocks/software/software.js
+++
@@ -1,5 +0,0 @@
-[
-  document.querySelector(".software__title"),
-  document.querySelector(".software__description")
-];
-document.querySelectorAll(".software__card-item");
