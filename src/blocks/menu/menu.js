64eb163156bae79c4fcb15c3a13cdfa4fa1a1ec0
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-04-24 18:51:55 +0300

    Implement close menu by click

--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,27 +1,53 @@
-export class MenuController{
-    constructor(menuElement){
-      const dropdownItems = menuElement.querySelectorAll('.menu__item:has(.menu)')  
-             
-        Array.from(dropdownItems).forEach(item=>{
-             console.log('dropdownItems',item)
-            this.processDropdownItem(item)
-        })
+export class MenuController {
+    constructor(menuElement) {
+        const dropdownItems = menuElement.querySelectorAll(
+            ".menu__item:has(.menu__dropdown)",
+        );
+
+        Array.from(dropdownItems).forEach((item) => {
+            console.log("dropdownItems", item);
+            this.processDropdownItem(item);
+        });
+        document.addEventListener("click", this.handleMenuItemClick);
     }
-    processDropdownItem(dropdownItem){
-        const arrow = document.createElement('div')
-        arrow.classList.add('menu__item-arrow') 
-        
-        dropdownItem.appendChild(arrow)
-        arrow.onclick = this.toggleDropdownMenu.bind(this,dropdownItem)
+
+    handleMenuItemClick(e) {
+        const target = e.target;
+
+        const menuItem = target.closest(".menu__item");
+        console.log("menuItem===", target, menuItem);
+        if (!menuItem) {
+            const menuList = document.querySelectorAll(".menu__dropdown");
+
+            return;
+        }
+        const dropdownMenu = menuItem.parentElement;
+        if (!dropdownMenu.classList.contains("menu__dropdown")) {
+            console.log("dropdownMenu===", dropdownMenu);
+            return;
+        }
+        if (dropdownMenu.classList.contains("menu__dropdown--close-on-click")) {
+            dropdownMenu.classList.remove("menu__dropdown--opened");
+        }
+
+        // menu__dropdown--close-on-click
     }
-    toggleDropdownMenu=(dropdownElement,e)=>{
-       
-        const menuElement = Array.from(dropdownElement.children).find(ch=>ch.classList.contains('menu'))
-         console.log('click arrow',dropdownElement.children,menuElement)
-        if(!menuElement){
-            return 
+    processDropdownItem(dropdownItem) {
+        const arrow = document.createElement("div");
+        arrow.classList.add("menu__item-arrow");
+
+        dropdownItem.appendChild(arrow);
+        arrow.onclick = this.toggleDropdownMenu.bind(this, dropdownItem);
+    }
+    toggleDropdownMenu = (dropdownElement, e) => {
+        const menuElement = Array.from(dropdownElement.children).find((ch) =>
+            ch.classList.contains("menu__dropdown"),
+        );
+        console.log("click arrow", dropdownElement.children, menuElement);
+        if (!menuElement) {
+            return;
         }
         // const isOpen = menuElement.classList.contains('menu--opened')
-        menuElement.classList.toggle('menu--opened')
-    }
+        menuElement.classList.toggle("menu__dropdown--opened");
+    };
 }
