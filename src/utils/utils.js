c25166b6df4bfe3ed40f93112c790b92036b0467
Pushkarniy Andrey <pushkarniy@epsa-spb.ru>
2026-05-08 16:46:29 +0300

    Add utils functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,22 @@
 export const toRem = (px) =>
     +(px / parseFloat(getComputedStyle(document.documentElement).fontSize));
+
+export function validateEmail(value) {
+    if (!value?.trim()) {
+        return false;
+    }
+    const isEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+        value,
+    );
+    return isEmail;
+}
+
+export function debounce(func, ms = 100) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, ms);
+    };
+}
